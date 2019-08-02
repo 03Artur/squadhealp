08e9ff7516f728797f7b5be208c7c7f2f53ded99
03Artur <artur.it2914@gmail.com>
2019-08-02 23:10:24 +0300

    commit from .sh file

--- quick-commit.sh
+++ quick-commit.sh
@@ -2,12 +2,14 @@
 if [ -n "$1" ]
 then
   git commit -a -m "$1"
+  git status
   else
     echo "Please input commit title"
 fi
 if [ -n "$2" ]
 then
   git push origin $2
+  git status
   else
     echo "Please input branch name"
 fi
