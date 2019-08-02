55e9736063d543e3759ff103d4a287fbf1d1bef0
03Artur <artur.it2914@gmail.com>
2019-08-02 23:09:26 +0300

    commit from .sh file

--- quick-commit.sh
+++ quick-commit.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-git add .
-git commit -m "$1"
-git push origin $2
+if [ -n "$1" ]
+then
+  git commit -a -m "$1"
+  else
+    echo "Please input commit title"
+fi
+if [ -n "$2" ]
+then
+  git push origin $2
+  else
+    echo "Please input branch name"
+fi
+
+
 
 
