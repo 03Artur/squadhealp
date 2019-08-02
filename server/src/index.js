e6ad44365280819ffc4dda510292cf39f5115aab
03Artur <artur.it2914@gmail.com>
2019-08-02 22:23:09 +0300

    commit from .sh file

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,6 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
 //static image files
@@ -24,4 +23,3 @@ app.use(router);
 app.use(errorHandler);
 
 app.listen(PORT);
-
